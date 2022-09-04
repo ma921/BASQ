7473aa356686a13933c759e44e712b53500b6adf
Masaki Adachi <61237927+ma921@users.noreply.github.com>
2022-09-04 16:17:58 +0900

    Add files via upload

--- BASQ/experiment/gmm.py
+++ BASQ/experiment/gmm.py
@@ -1,9 +1,11 @@
 import torch
 from torch.distributions.multivariate_normal import MultivariateNormal
 
+
 def random_choice(pop_tensor, num_samples, device):
     """Use torch.randperm to generate indices on a GPU tensor."""
     return pop_tensor[torch.randperm(len(pop_tensor), device=device)[:num_samples]]
+
 
 class GMM:
     def __init__(self, dim, mu_pi, cov_pi, device):
@@ -12,13 +14,13 @@ class GMM:
         self.mu_pi = mu_pi
         self.cov_pi = cov_pi
         self.initialising()
-        
+
     def initialising(self):
         self.n_comp = self.component_generator()
         self.means = torch.stack([self.mean_generator() for _ in range(self.n_comp)]).to(self.device)
         self.cov = self.cov_generator()
         self.weights = self.weights_calc()
-        
+
     def IO(self, x):
         if len(x.size()) == 1:
             return True
@@ -28,21 +30,21 @@ class GMM:
             return False
 
     def mean_generator(self):
-        return (3*(2*torch.rand(self.dim) - 1)).to(self.device)
+        return (3 * (2 * torch.rand(self.dim) - 1)).to(self.device)
 
     def cov_generator(self):
-        return torch.diag(3*torch.rand(self.dim) + 1).to(self.device)
+        return torch.diag(3 * torch.rand(self.dim) + 1).to(self.device)
 
     def component_generator(self):
-        return random_choice(torch.arange(10,16), 1, self.device).item()
-    
+        return random_choice(torch.arange(10, 16), 1, self.device).item()
+
     def weights_calc(self):
         Npdfs = MultivariateNormal(
             self.mu_pi,
             self.cov_pi + self.cov,
         ).log_prob(self.means).exp().to(self.device)
-        return (1/Npdfs)/self.n_comp
-    
+        return (1 / Npdfs) / self.n_comp
+
     def __call__(self, x):
         if self.IO(x):
             Npdfs = MultivariateNormal(
@@ -50,14 +52,14 @@ class GMM:
                 self.cov,
             ).log_prob(self.means - x).exp()
             return torch.sum(self.weights * Npdfs).to(self.device)
-        
+
         else:
             d_x = len(x)
-            x = (torch.tile(self.means, (d_x,1,1)) - x.unsqueeze(1)).reshape(self.n_comp * d_x, self.dim).to(self.device)
+            x = (torch.tile(self.means, (d_x, 1, 1)) - x.unsqueeze(1)).reshape(self.n_comp * d_x, self.dim).to(self.device)
             Npdfs = MultivariateNormal(
                 torch.zeros(self.dim).to(self.device),
                 self.cov,
             ).log_prob(x).exp().reshape(d_x, self.n_comp).to(self.device)
-            
+
             f = self.weights.unsqueeze(0) * Npdfs
             return torch.sum(f, axis=1).to(self.device)
